9a461494e0ab22f5482a04ebcbcae246a3323883
samuelhfdias-prog <samuelhfdias-prog@users.noreply.github.com>
2026-05-25 12:37:02 -0300

    Fix: remove provideZoneChangeDetection to fix NG0908 white screen

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
@@ -6,7 +6,7 @@ import { jwtInterceptor } from './services/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([jwtInterceptor])),
   ]
